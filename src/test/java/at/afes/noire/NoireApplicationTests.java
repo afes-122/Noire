d85a8bbb0011b152519888d82fce40deeb211b7c
Alexander Rziha <alexander.rziha@hotmail.com>
2024-04-07 21:14:35 +0200

    first commit

---
+++ src/test/java/at/afes/noire/NoireApplicationTests.java
@@ -0,0 +1,13 @@
+package at.afes.noire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
